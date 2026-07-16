50e58b63efbb88beeed5f73bc71e992be3a0871f
amardito <amardito37@gmail.com>
2026-07-17 03:30:51 +0700

    chore: remove tracked node_modules and dist, add .gitignore

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './events/types';
-export * from './alerts/formatter';
-export * from './alerts/pipeline';
-export * from './delivery/discord';
-export * from './util/dedupe';
-//# sourceMappingURL=index.d.ts.map
